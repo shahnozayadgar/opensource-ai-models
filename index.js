0b20180fc5c3f8e6606f4ad52d3c32df0b487862
shahnozayadgar <yadgarova@kaist.ac.kr>
2025-02-20 22:08:42 +0900

    text generation

---
+++ index.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { HfInference } from '@huggingface/inference';
+
+const hf = new HfInference(process.env.HF_TOKEN)
+
+const textToGenerate = "The definition of machine learning is "
+
+const response = await hf.textGeneration({
+    inputs: textToGenerate,
+    model: "HuggingFaceH4/zephyr-7b-beta"
+})
+
+console.log(response)
+
