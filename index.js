6fe904c2bfa1dc16b49318ec96ce7e0cc9189208
shahnozayadgar <yadgarova@kaist.ac.kr>
2025-02-20 22:15:32 +0900

    text classification

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ import { HfInference } from '@huggingface/inference';
 
 const hf = new HfInference(process.env.HF_TOKEN)
 
-const textToGenerate = "The definition of machine learning is "
+// const textToGenerate = "The definition of machine learning is "
 
-const response = await hf.textGeneration({
-    inputs: textToGenerate,
-    model: "HuggingFaceH4/zephyr-7b-beta"
+// const response = await hf.textGeneration({
+//     inputs: textToGenerate,
+//     model: "HuggingFaceH4/zephyr-7b-beta"
+// })
+
+// console.log(response)
+
+const testToClassify = "I just bought a new camera. It is the best camera I have ever owned!"
+
+const repsonse = await hf.textClassification({
+    model: "distilbert-base-uncased-finetuned-sst-2-english",
+    inputs: testToClassify
 })
 
-console.log(response)
+console.log(repsonse)
 
