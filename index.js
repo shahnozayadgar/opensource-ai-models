6c24a9c416452b682ff44bb7f4b4794cad49233f
shahnozayadgar <yadgarova@kaist.ac.kr>
2025-02-25 20:09:56 +0900

    transformers in browser

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import { HfInference } from '@huggingface/inference';
-import { listModels} from "@huggingface/hub";
+// import dotenv from 'dotenv';
+// dotenv.config();
+// import { HfInference } from '@huggingface/inference';
+// import { listModels} from "@huggingface/hub";
+import {pipeline} from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.8.0'
 
-const hf = new HfInference(process.env.HF_TOKEN)
+
+// const hf = new HfInference(process.env.HF_TOKEN)
 
 // const textToGenerate = "The definition of machine learning is "
 
@@ -61,35 +63,101 @@ const hf = new HfInference(process.env.HF_TOKEN)
 // const newImage = document.getElementById("new-image")
 // newImage.src = newImageBase64
 
-async function isModelInferenceEnabled(modelName) {
-    const response = await fetch(`https://api-inference.huggingface.co/status/${modelName}`, {
-        headers: {
-            'Authorization': `Bearer ${process.env.HF_TOKEN}`
-        }
+// async function isModelInferenceEnabled(modelName) {
+//     const response = await fetch(`https://api-inference.huggingface.co/status/${modelName}`, {
+//         headers: {
+//             'Authorization': `Bearer ${process.env.HF_TOKEN}`
+//         }
+//     })
+//     const data = await response.json()
+//     return data.state == "Loadable"
+// }
+
+// const models = []
+
+// for await (const model of listModels({
+//     credentials:{
+//         accessToken: process.env.HF_TOKEN
+//     },
+//     search: {
+//         task: "text-to-image"
+//     }
+// })) {
+//     if (model.likes < 2000) {
+//         continue
+//     }
+//     if (await isModelInferenceEnabled(model.name)) {
+//         models.push(model)
+//     }
+// }
+
+// models.sort((model1, model2) => model2.likes - model1.likes)
+// for (const model of models) {
+//     console.log(`${model.likes} Likes: http://huggingface.co/${model.name} `)
+// }
+
+const status = document.getElementById('status')
+const image = document.getElementById('image')
+const detectObjectsButton = document.getElementById('detect-objects')
+const imageContainer = document.getElementById('image-container')
+
+//loading the model and creating a new object detection pipeline
+status.textContent = "Loading model..."
+const detector = await pipeline('object-detection', 'Xenova/yolos-tiny')
+
+//enabling object detection
+detectObjectsButton.addEventListener('click', detectAndDrawObjects)
+detectObjectsButton.disabled = false
+status.textContent = 'Ready'
+
+async function detectAndDrawObjects() {
+    status.textContent = 'Detecting...'
+    const detectedObjects = await detector(image.src, {
+        threshold: 0.95,
+        percentage: true
     })
-    const data = await response.json()
-    return data.state == "Loadable"
+
+    status.textContent = 'Drawing...'
+    detectedObjects.forEach(obj => {
+        drawObjectBox(obj)})
+    
+    status.textContent = 'Done!'
 }
 
-const models = []
+function drawObjectBox(detectedObject) {
+    const {label, score, box} = detectedObject
+    const {xmax, xmin, ymax, ymin} = box
+    //generating a random color for the box
+    const color = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, 0)
 
-for await (const model of listModels({
-    credentials:{
-        accessToken: process.env.HF_TOKEN
-    },
-    search: {
-        task: "text-to-image"
-    }
-})) {
-    if (model.likes < 2000) {
-        continue
-    }
-    if (await isModelInferenceEnabled(model.name)) {
-        models.push(model)
-    }
+    //drawing the box
+    const boxElement = document.createElement('div')
+    boxElement.className = 'bounding-box'
+    Object.assign(boxElement.style, {
+        borderColor: color,
+        left: 100 * xmin + '%',
+        top: 100 * ymin + '%',
+        width: 100 * (xmax - xmin) + '%',
+        height: 100 * (ymax - ymin) + '%'
+    })
+
+    //drawing label 
+    const labelElement = document.createElement('span')
+    labelElement.textContent = `${label} (${Math.floor(100 * score)}%)`
+    labelElement.className = 'bounding-box-label'
+    labelElement.style.backgroundColor = color
+    boxElement.appendChild(labelElement)
+    imageContainer.appendChild(boxElement)
 }
 
-models.sort((model1, model2) => model2.likes - model1.likes)
-for (const model of models) {
-    console.log(`${model.likes} Likes: http://huggingface.co/${model.name} `)
-}
+// status.textContent = 'Detecting Objects...'
+
+// const detectedObjects = await detector(image.src, {
+//     threshold: 0.5,
+//     percentage: true
+// })
+// detectedObjects.forEach(object => {
+//     console.log(object) 
+// })
+
+// status.textContent = 'Done!'
